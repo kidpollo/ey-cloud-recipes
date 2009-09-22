f69d3d8fb9f190a3bd9642daba9eba06c5037b3e
@kidpollo <kidpollo@gmail.com>
2009-09-21 21:19:54 -0500

    couchdb

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@ execute "testing" do
 end
 
 # uncomment if you want to run couchdb recipe
-# require_recipe "couchdb"
+require_recipe "couchdb"
 
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
 # require_recipe "mbari-ruby"
