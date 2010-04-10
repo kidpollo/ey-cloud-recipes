ac51c721125bb2d4ddd0f215106be39f2c3bdd6b
@kidpollo <kidpollo@gmail.com>
2010-04-10 12:16:06 -0500

    changed

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -11,7 +11,7 @@
 # require_recipe "couchdb"
 
 # uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
-require_recipe "mbari-ruby"
+#require_recipe "mbari-ruby"
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # require_recipe "sphinx"
